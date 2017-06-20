4d09b2c20b0e09fe6a88c4405df4aceeb233324a
Fossaert Thomas <thomas.fossaert@epitech.eu>
2017-06-20 13:51:11 +0200

    Init ia

---
+++ src/ai/ZappException.cpp
@@ -0,0 +1,20 @@
+/*
+** ZappException.cpp for PSU_2016_zappy in /home/fossae_t/rendu2/PSU_2016_zappy/src/ai
+**
+** Made by Thomas Fossaert
+** Login   <fossae_t@epitech.net>
+**
+** Started on  Tue Jun 20 12:03:07 2017 Thomas Fossaert
+** Last update Tue Jun 20 12:03:10 2017 Thomas Fossaert
+*/
+
+#include "ZappException.hpp"
+
+ZappException::ZappException(std::string msg) throw() : _msg(msg)
+{
+}
+
+const char *ZappException::what() const throw()
+{
+  return (_msg.c_str());
+}
