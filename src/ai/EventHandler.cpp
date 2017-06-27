57f2de69a3a3da5686f92e9e8bab5edd5bdc5aaa
Adrien Warin <adrien.warin@epitech.eu>
2017-06-27 14:40:56 +0200

    check case requirement

--- src/ai/EventHandler.cpp
+++ src/ai/EventHandler.cpp
@@ -5,7 +5,7 @@
 ** Login   <warin_a@epitech.net>
 **
 ** Started on  Tue Jun 20 15:00:59 2017 Adrien Warin
-** Last update Tue Jun 27 12:32:43 2017 Thomas Fossaert
+** Last update Tue Jun 27 14:20:36 2017 Adrien Warin
 */
 
 #include "EventHandler.hpp"
@@ -145,18 +145,25 @@ void EventHandler::launchScript()
           {
             LookAround();
             TakeEverything();
-            PutRock("linemate", this->_inventory["linemate"], this->_need["linemate"]);
-            PutRock("deraumere", this->_inventory["deraumere"], this->_need["deraumere"]);
-            PutRock("sibur", this->_inventory["sibur"], this->_need["sibur"]);
-            PutRock("mendiane", this->_inventory["mendiane"], this->_need["mendiane"]);
-            PutRock("phiras", this->_inventory["phiras"], this->_need["phiras"]);
-            PutRock("thystame", this->_inventory["thystame"], this->_need["thystame"]);
+            PutRequirementRock("linemate");
+            PutRequirementRock("deraumere");
+            PutRequirementRock("sibur");
+            PutRequirementRock("mendiane");
+            PutRequirementRock("phiras");
+            PutRequirementRock("thystame");
+            // PutRock("linemate", this->_inventory["linemate"], this->_need["linemate"]);
+            // PutRock("deraumere", this->_inventory["deraumere"], this->_need["deraumere"]);
+            // PutRock("sibur", this->_inventory["sibur"], this->_need["sibur"]);
+            // PutRock("mendiane", this->_inventory["mendiane"], this->_need["mendiane"]);
+            // PutRock("phiras", this->_inventory["phiras"], this->_need["phiras"]);
+            // PutRock("thystame", this->_inventory["thystame"], this->_need["thystame"]);
             //SetObject("linemate");
             Incantation();
             _currentState = State::INCANTATION;
           }
         else if (_currentState == State::NORMAL)
           {
+            Inventory();
             LookAround();
             MoveUp();
             random_variable = std::rand();
@@ -172,6 +179,7 @@ void EventHandler::launchScript()
             if (isAbleToIncant() == true)
               _currentState = State::READYFORINC;
           }
+        std::cout << "LEVEL = " << _level << "\n";
         std::cout << "FOOD: " << _inventory["food"] << '\n';
         std::cout << "Linemate: " << _inventory["linemate"] << '\n';
         std::cout << "deraumere: " << _inventory["deraumere"] << '\n';
@@ -264,6 +272,35 @@ void EventHandler::parseTiles(const std::string & tiles)
     }
 }
 
+void EventHandler::PutRequirementRock(const std::string &item)
+{
+    int nb = 0;
+    int nb_to_put = 0;
+
+    nb = CaseRequirement(item, 0);
+    if (this->_need[item] > 0)
+        nb_to_put = this->_need[item] - nb;
+    std::cout << "NOMBRE DE " << item << " A POSER = " << nb_to_put << '\n';
+    PutRock(item, this->_inventory[item], nb_to_put);
+}
+
+int EventHandler::CaseRequirement(const std::string & item, int tileNbr)
+{
+  int cpt = 0;
+  int rock = 0;
+
+  for (auto it : _tiles)
+    {
+      for (auto it2 : _tiles[cpt])
+        {
+          if (cpt == tileNbr && it2 == item)
+            rock++;
+        }
+      cpt++;
+    }
+  return (rock);
+}
+
 bool EventHandler::isAbleToIncant()
 {
   if (_inventory["linemate"] >= _need["linemate"] &&
@@ -318,7 +355,7 @@ void EventHandler::Inventory()
 {
   _sock->sendMsg("Inventory\n");
   _sock->recvMsg();
-  std::cout << "INVENTORY : " << _sock->getLastMsg() << '\n';
+  std::cout << " ---- INVENTORY : " << _sock->getLastMsg() << '\n';
   /*if (has_any_digits(_sock->getLastMsg()) == true &&
       (_sock->getLastMsg() != "ko\n" ||
       _sock->getLastMsg() != "ok\n"))*/
