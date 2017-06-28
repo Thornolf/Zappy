e57ea2fa846ac69b140e098a4eb44691512c9483
stasza_p <pierre.staszak@epitech.eu>
2017-06-28 13:32:46 +0200

    inventory send_socket

--- src/server/command_inventory.c
+++ src/server/command_inventory.c
@@ -5,7 +5,7 @@
 ** Login   <pierre@epitech.net>
 **
 ** Started on  Thu Jun 22 14:33:04 2017 Pierre
-** Last update Wed Jun 28 13:15:03 2017 Pierre
+** Last update Wed Jun 28 13:32:34 2017 Pierre
 */
 
 #include <string.h>
@@ -28,15 +28,33 @@ t_stuff		*init_stuff(void)
   return (stuff);
 }
 
-void		print_stuff(t_stuff *stuff)
+char *int_to_string(int nb)
 {
-  printf("[food %d, ", stuff->quantities[FOOD]);
-  printf("linemate %d, ", stuff->quantities[LINEMATE]);
-  printf("deraumere %d, ", stuff->quantities[DERAUMERE]);
-  printf("sibur %d, ", stuff->quantities[SIBUR]);
-  printf("mendiane %d, ", stuff->quantities[MENDIANE]);
-  printf("phiras %d, ", stuff->quantities[PHIRAS]);
-  printf("thystame %d]\n", stuff->quantities[THYSTAME]);
+  char *str;
+
+  if ((str = malloc(sizeof(char) * 4)) == NULL)
+    return (NULL);
+  sprintf(str, "%d", nb);
+  return (str);
+}
+
+void		print_stuff(int fd, t_stuff *stuff)
+{
+  send_socket(fd, "[food ");
+  send_socket(fd, int_to_string(stuff->quantities[FOOD]));
+  send_socket(fd, ", linemate ");
+  send_socket(fd, int_to_string(stuff->quantities[LINEMATE]));
+  send_socket(fd, ", deraumere ");
+  send_socket(fd, int_to_string(stuff->quantities[DERAUMERE]));
+  send_socket(fd, ", sibur ");
+  send_socket(fd, int_to_string(stuff->quantities[SIBUR]));
+  send_socket(fd, ", mendiane ");
+  send_socket(fd, int_to_string(stuff->quantities[MENDIANE]));
+  send_socket(fd, ", phiras ");
+  send_socket(fd, int_to_string(stuff->quantities[PHIRAS]));
+  send_socket(fd, ", thystame ");
+  send_socket(fd, int_to_string(stuff->quantities[THYSTAME]));
+  send_socket(fd, "]\n");
 }
 
 void command_inventory(t_server *server, t_client *client)
@@ -50,5 +68,5 @@ void command_inventory(t_server *server, t_client *client)
     printf("error : player not found\n");
     return ;
   }
-  print_stuff(player->stuff);
+  print_stuff(player->fd, player->stuff);
 }
